0d44206b9d2332a0311fe95addf2457c6b17ca70
Fabien Crespel <fabien@crespel.net>
2021-03-13 01:06:25 +0100

    Add backups bucket

--- storage.tf
+++ storage.tf
@@ -1,4 +1,11 @@
 // GCS bucket for backups
+resource "google_storage_bucket" "gcs_bucket_backups" {
+  name                        = "${var.gcp_project_id}-backups"
+  project                     = var.gcp_project_id
+  location                    = "europe-west1"
+  storage_class               = "COLDLINE"
+  uniform_bucket_level_access = true
+}
 resource "google_storage_bucket" "gcs_bucket_nextcloud" {
   name                        = "${var.gcp_project_id}-nextcloud"
   project                     = var.gcp_project_id
